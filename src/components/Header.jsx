6375634dba1f7990812deba55b1d59d186465f4a
Samridhi <sharmasamridhi68@gmail.com>
2025-06-30 23:03:44 +0530

    main page

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import logo from "../images/s.white.withoutBG.png";
+
+function Header(){
+    return(
+        <div className="navbar">
+            <img src={logo} alt="Samridhi Sharma Logo"/>
+            <ul>
+                <li>Home</li>
+                <li>Skills</li>
+                <li>Experience</li>
+                <li>Projects</li>
+                <li>Education</li>
+                <li>Contact Me</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/components/Home.jsx
@@ -0,0 +1,40 @@
+import image1 from "../images/imgm1.png";
+import image2 from "../images/imgm2.png";   
+import image3 from "../images/imgm3.png";
+import image4 from "../images/imgm4.png";
+
+function Home(){
+    return(
+        <div className="home">
+            <div className="home_image">
+                <div className="home_parallax_img1">
+                    <img src={image1} alt="first background layer"/>
+                </div>
+                <div className="home_parallax_img2">
+                    <img src={image2} alt="second background layer"/>
+
+                </div>
+                <div className="home_parallax_img3">
+                    <img src={image3} alt="third background layer"/>
+
+                </div>
+                <div className="home_parallax_img4">
+                    <img src={image4} alt="fourth background layer"/>
+                </div>
+            </div>
+
+            <div className="home_content">
+                <h1>SAMRIDHI SHARMA</h1>
+                <p>Web developer</p>
+            </div>
+
+            <div className="home_scroll">
+                <a href="#about">
+                    <i class="bx bx-mouse"></i>
+                </a>
+            </div>
+        </div>
+    );
+}
+
+export default Home;    
