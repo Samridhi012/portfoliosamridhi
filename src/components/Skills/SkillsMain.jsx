dfe15672ddf91dd0c47bef309d7906650f67d0ce
Samridhi <sharmasamridhi68@gmail.com>
2025-07-03 13:58:34 +0530

    skillsDONE

--- src/components/Skills/SkillsMain.jsx
+++ src/components/Skills/SkillsMain.jsx
@@ -2,15 +2,15 @@ import SkillDisplay from "./SkillDisplay";
 
 function Skills(){
     return(
-        <div className="skills">
-            {/* <h1>SKILLS</h1> */}
+        <section className="skills">
+            <h1>SKILLS</h1>
             <div className="skill-display">
                 <SkillDisplay name="Frontend" />
                 <SkillDisplay name="Backend" />
                 <SkillDisplay name="Database" />
                 <SkillDisplay name="Others" />
             </div>
-        </div>
+        </section>
     );
 }
 
