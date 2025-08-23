35681439b3878ef669f1968a086ccdd2323b6e48
Samridhi <sharmasamridhi68@gmail.com>
2025-08-23 18:16:47 +0530

    experience section refined

--- src/components/Experience/Exp.js
+++ src/components/Experience/Exp.js
@@ -4,13 +4,13 @@ export const expData = [
         "location" : "Remote",
         "position" : "Contributor",
         "modeAttended" : "Online",
-        "timePeriod" : "'25 - '25",
+        "timePeriod" : "Aug'25 - Oct'25",
         "description" : "Collaborated to develop and improve open-source projects. Demonstrated problem-solving abilities through code contributions and technical solutions."
     },
     {
         "company" : "Apptechies",
         "location" : "Mohali, Punjab",
-        "position" : "Trainee",
+        "position" : "Web dev Trainee",
         "modeAttended" : "On-site",
         "timePeriod" : "Jun'25 - Jul'25",
         "description" : "Attended training to sharpen my web development skills and techniques. Did hands-on practice in using web development principles to different projects."
@@ -26,7 +26,7 @@ export const expData = [
     {
         "company" : "Excellence Technology",
         "location" : "Mohali, Punjab",
-        "position" : "Trainee",
+        "position" : "Web dev Trainee",
         "modeAttended" : "On-site",
         "timePeriod" : "Jun'24 - Jul'24",
         "description" : "Attended training to build a strong understanding of frontend development processes and techniques."
@@ -34,7 +34,7 @@ export const expData = [
     {
         "company" : "Acmegrade",
         "location" : "Remote",
-        "position" : "Trainee",
+        "position" : "Web dev Trainee",
         "modeAttended" : "Online",
         "timePeriod" : "Aug'24 - Sept'24",
         "description" : "Completed comprehensive training modules to gain proficiency in industry-specific software. Developed proficiency in core web development technologies."
