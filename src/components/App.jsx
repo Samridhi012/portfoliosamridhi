d24e663141fb209c6b006b78070291891a704879
Samridhi <sharmasamridhi68@gmail.com>
2025-08-27 00:17:29 +0530

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,25 @@ import Header from "./Header/Header";
 import Home from "./Home/Home";
 import Skills from "./Skills/SkillsMain";
 import Experience from "./Experience/ExperienceMain";
+import {useState, useEffect} from "react";
+import {useLocation} from "react-router-dom";
 
 function App() {
+  const [showResumeBtn, setShowResumeBtn] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    setShowResumeBtn(location.pathname !== "/");
+  }, [location.pathname]);
+
   return (
     <div className="app">
       <Header />
       <Home />
+
+      {/* Conditional rendering of the resume button based on the current route */}
+      {(showResumeBtn === true) ? (<a className="resume-btn" href="https://www.google.com"><span>Download Resume</span></a>) : null}
+
       <Skills />
       <Experience />
     </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,6 +13,13 @@ function Header(){
                 <li>Education</li>
                 <li>Contact Me</li>
             </ul>
+            {/* <button class="custom-btn btn-3"><span>Read More</span></button> */}
+
+            {/* <a className="resume-btn" href="#google link">
+                <span>Download Resume</span>
+            </a> */}
+
+            <a href="https://www.google.com"><button class="resume-btn"><span>Download Resume</span></button></a>
         </div>
     );
 }
